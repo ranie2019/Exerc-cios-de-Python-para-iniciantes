49d8b0bbcf3b92f859dbde89871a15aa286f3b35
Ranie <ranie.black29@gmail.com>
2020-11-05 12:20:16 -0300

    Exercicios Basicos de python

---
+++ Entrada de personagem .py
@@ -0,0 +1,5 @@
+nome = str(input('Digite o Seu Nome: '))
+idade = int(input('Digite Sua Idade: '))
+soma = 100 - idade
+
+print(f'{nome} com {idade} tera 100 anos daqui a {soma} anos')
---
+++ Ímpar ou par.py
@@ -0,0 +1,6 @@
+numero = int(input('digite um numero: '))
+soma = numero % 2
+if soma == 0:
+    print('O Numero e par')
+else:
+    print('O Numero e Impar')
